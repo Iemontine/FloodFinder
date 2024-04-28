261375c3a99c78e7ef3b7b962e88fc1a9a59faa2
Andrew Yeow <acyeow@ucdavis.edu>
2024-04-27 18:22:21 -0700

    added helpers

---
+++ helpers.py
@@ -0,0 +1,25 @@
+import os
+from dotenv import load_dotenv
+import http.client
+import json
+
+load_dotenv()
+browserless_api_key = os.getenv('BROWSERLESS_API_KEY')
+serper_api_key = os.getenv('SERPER_API_KEY')
+
+def search(query):
+    conn = http.client.HTTPSConnection("google.serper.dev")
+    payload = json.dumps({
+    "q": query
+    })
+    headers = {
+        'X-API-KEY': serper_api_key,
+        'Content-Type': 'application/json'
+    }
+    conn.request("POST", "/search", payload, headers)
+    res = conn.getresponse()
+    data = (json.loads(res.read().decode("utf-8")))
+    print(data['organic'][0]['snippet'])
+    
+search("What are the coordinates of Los Angles, CA?")
+
