3aaa07164cfeb3e458d94bf5da1f7dda947c5435
Darroll <computereality@gmail.com>
2024-04-28 10:01:21 -0700

    python scripts semi-final changes

--- src/web.py
+++ src/web.py
@@ -4,13 +4,12 @@ import http.client
 import json
 
 load_dotenv()
-browserless_api_key = os.getenv('BROWSERLESS_API_KEY')
 serper_api_key = os.getenv('SERPER_API_KEY')
 
-def search_web(location):
+def research_coordinates(location):
     conn = http.client.HTTPSConnection("google.serper.dev")
     payload = json.dumps({
-    "q": f"coordinates of {location}"
+   		"q": f"coordinates of {location}"
     })
     headers = {
         'X-API-KEY': serper_api_key,
